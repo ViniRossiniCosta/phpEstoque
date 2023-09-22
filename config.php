a801bd55cfa39f6d3359d3631e33c87d1e402cad
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-22 11:28:22 -0300

    atualizando o projeto com novos recursos do mvc

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+require 'environment.php';
+
+global $config;
+global $db;
+define("BASE_URL", "http://localhost:8080/simples/arq_mvc/");
+
+$config = array();
+if(ENVIRONMENT == 'development') {
+	$config['dbname'] = 'estoque_vini';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+} else {
+	$config['dbname'] = 'nova_loja';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+}
+
+$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
